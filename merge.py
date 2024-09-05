519ee54bbc1e66e75e27a7432baa67f0450880b6
Gabe Righi <gaberighi@gmail.com>
2024-09-05 10:03:01 -0500

    Removed files

--- merge.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-
-cards = pd.read_csv("Files/cards.csv")
-tokens = pd.read_csv("Files/tokens.csv")
-
-allCards = cards.merge(tokens, how='outer')
-allCards.head(5)
-allCards.to_csv("Files/merge.csv")
