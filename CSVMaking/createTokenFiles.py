11b1347a21b9d9e68924db6120d833257ab31469
Gabe Righi <gaberighi@gmail.com>
2024-09-20 10:45:50 -0500

    Fixed how related cards work due to SISSAY

--- CSVMaking/createTokenFiles.py
+++ CSVMaking/createTokenFiles.py
@@ -1,21 +1,29 @@
-import pandas as pd
-import json
+# import pandas as pd
+# import json
+# DEPRECATED
+# import pandas as pd
+# import json
+# uuidToRelatedCards = {}
+# uuidToName = {}
+# def related_cards_to_uuid(row):
+# 	try:
+# 		uuidToName[row["uuid"]] = row["name"] 
+# 		if(row["name"]  == "Treasure"):
+# 			print("Jalapeno")
+# 		for name in json.loads(row["relatedCards"])["reverseRelated"]:
+# 			name = name.strip()
+# 			if(row["name"]  == "Treasure"):
+# 				print(name)
+# 			uuidToRelatedCards[name] = uuidToRelatedCards.get(name, [])
+# 			uuidToRelatedCards[name].append(row["uuid"].strip())
+# 	except Exception as e:
+# 		if(row["name"]  == "Treasure"):
+# 			print(e)
+# 		pass
 
-uuidToRelatedCards = {}
-uuidToName = {}
-def related_cards_to_uuid(row):
-	try:
-		uuidToName[row["uuid"]] = row["name"] 
-		for name in json.loads(row["relatedCards"])["reverseRelated"]:
-			name = name.strip()
-			uuidToRelatedCards[name] = uuidToRelatedCards.get(name, [])
-			uuidToRelatedCards[name].append(row["uuid"].strip())
-	except:
-		pass
-
-df = pd.read_csv("ParsedCSV/tokens.csv")
-df.apply(related_cards_to_uuid, axis=1)
-uuidToRelatedCardsFile = open("relatedTokens.txt", "w")
-uuidToRelatedCardsFile.write(json.dumps(uuidToRelatedCards))
-uuidToNameFile = open("uuidToName.txt", "w")
-uuidToNameFile.write(json.dumps(uuidToName))
+# df = pd.read_csv("ParsedCSV/tokens.csv")
+# df.apply(related_cards_to_uuid, axis=1)
+# uuidToRelatedCardsFile = open("relatedTokens.txt", "w")
+# uuidToRelatedCardsFile.write(json.dumps(uuidToRelatedCards))
+# uuidToNameFile = open("uuidToName.txt", "w")
+# uuidToNameFile.write(json.dumps(uuidToName))
